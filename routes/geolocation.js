0908c2c8ab2e9f8a694d7cec37cfa6b8c7157537
hvail <415195993@qq.com>
2018-03-28 14:34:07 +0800

    v1.0.1.0

--- routes/geolocation.js
+++ routes/geolocation.js
@@ -3,32 +3,32 @@
  */
 const key = process.env.GOOGLE_KEY || "AIzaSyBGCpcpnrwlRI1j24x7K1Mhui44XBLQ6co";
 const url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + key;
-// const request = require('request').defaults({'proxy': 'http://127.0.0.1:2080'});
-const request = require('request');
+const request = require('request').defaults({'proxy': 'http://127.0.0.1:2080'});
+// const request = require('request');
 
 const express = require('express');
 const router = express.Router();
 
 /** 有效性的测试 **/
-let body = {
-    considerIp: "false",
-    wifiAccessPoints: [
-        {
-            macAddress: "d0:ee:07:1e:ff:d2"
-        },
-        {
-            macAddress: "14:75:90:c5:b1:52"
-        },
-        {
-            macAddress: "d0:ee:07:3a:bf:e0"
-        }
-    ]
-};
+// let body = {
+//     considerIp: "false",
+//     wifiAccessPoints: [
+//         {
+//             macAddress: "d0:ee:07:1e:ff:d2"
+//         },
+//         {
+//             macAddress: "14:75:90:c5:b1:52"
+//         },
+//         {
+//             macAddress: "d0:ee:07:3a:bf:e0"
+//         }
+//     ]
+// };
 
 const _buildWifiBody = function (wifi, ct) {
     let rexWIFI = /(.*?)\*(.*?)\*(.*?)(\*|$)/g;
     let rexCT = /(.*?)\*(.*?)\*(.*?)\*(.*?)$/g;
-    let result = {considerIp: "false", wifiAccessPoints: []};
+    let result = {considerIp: "false", wifiAccessPoints: [], cellTowers: []};
     if (wifi) {
         while (rexWIFI.test(wifi)) {
             let obj = {
@@ -36,20 +36,22 @@ const _buildWifiBody = function (wifi, ct) {
                 signalStrength: RegExp.$2 * 1,
                 channel: RegExp.$3 * 1
             };
-            console.log(RegExp.$1);
-            console.log(RegExp.$2);
-            console.log(RegExp.$3);
             result.wifiAccessPoints.push(obj);
         }
     }
     if (ct) {
         rexCT.test(ct);
+        result.cellTowers.push({
+            cellId: RegExp.$3,
+            locationAreaCode: RegExp.$2,
+            mobileCountryCode: RegExp.$1,
+            mobileNetworkCode: RegExp.$4
+        });
     }
     return result;
 };
 
 const getWIFILocation = function (wifi) {
-    console.log(wifi);
     return new Promise(function (resolve, reject) {
         request({url: url, method: "POST", json: wifi}, function (err, response, body) {
             if (err) {
