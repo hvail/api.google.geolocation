f14904f3f097caeb0df6aff9e0b0419287aa6335
hvail <415195993@qq.com>
2018-05-17 12:14:00 +0800

    v1.2.0.1-公开接口

--- routes/cell.js
+++ routes/cell.js
@@ -2,9 +2,12 @@
  * Created by hvail on 2018/5/16.
  */
 const express = require('express');
+const request = require('request');
+const util = require('util');
 const redis = require('./../my_modules/redishelp');
 const router = express.Router();
 const fnOK = (req, res) => res.send("OK");
+const base_url = "http://api.map.baidu.com/timezone/v1?coord_type=wgs84ll&location=%s,%s&timestamp=%s&ak=inl7EljWEdaPIiDKoTHM3Z7QGMOsGTDT";
 
 const cellRes = (poi) => {
     return {
@@ -21,10 +24,35 @@ const getCt = (req, res) => {
     let {mcc, mnc, lac, cid} = req.params;
     let key = `${mcc}:${lac}-${cid}`;
     redis.geopos("CellTowerLocationHash", key, (err, pos) => {
+        if (!err && pos.length > 0) {
+            _getTimeByLLC(pos[0][1], pos[0][0], function (data) {
+                let _tz = JSON.parse(data);
+                res.send({
+                    TimeZoneId: _tz.timezone_id,
+                    TimeZone: _tz.raw_offset / 3600
+                });
+            });
+        } else
+            res.send({})
+    });
+};
+
+const getTz = (req, res) => {
+    let {mcc, mnc, lac, cid} = req.params;
+    let key = `${mcc}:${lac}-${cid}`;
+    redis.geopos("CellTowerLocationHash", key, (err, pos) => {
         if (!err && pos.length > 0)
             res.send(cellRes(pos[0]));
         else
-            res.send({})
+            res.send(200, "");
+    });
+};
+
+let _getTimeByLLC = function (lat, lng, cb) {
+    let tick = Math.round(new Date().getTime() / 1000);
+    let url = util.format(base_url, lat, lng, tick);
+    request(url, function (err, response, body) {
+        cb && cb(body);
     });
 };
 
@@ -32,5 +60,6 @@ const getCt = (req, res) => {
 router.get('/', fnOK);
 
 router.get('/q/:mcc/:mnc/:lac/:cid', getCt);
+router.get('/tz/:mcc/:mnc/:lac/:cid', getCt);
 
 module.exports = router;
