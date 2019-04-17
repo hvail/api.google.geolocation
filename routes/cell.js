e64a5e6af05e1df7b8e64d6a4cc7c8b4883ea4fc
hvail <415195993@qq.com>
2019-04-17 16:00:56 +0800

    v2.1.7-fix npm lib

--- routes/cell.js
+++ routes/cell.js
@@ -122,7 +122,7 @@ let _readRemoteWifi = (mcc, mnc, lac, cid, wifi) => {
         .then(lbs => {
             if (lbs.infocode === '10000') {
                 let ls = lbs.result.location.split(",");
-                ls = console.log(offset.gg_to_wgs84({Lat: ls[1] * 1, Lng: ls[0] * 1}));
+                ls = offset.gg_to_wgs84({Lat: ls[1] * 1, Lng: ls[0] * 1});
                 let result = {
                     "Latitude": ls[1].toFixed(6), "Longitude": ls[0].toFixed(6), "Range": lbs.result.radius,
                     "latitude": ls[1].toFixed(6), "longitude": ls[0].toFixed(6), "Signal": -85
