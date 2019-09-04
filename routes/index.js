8a921dd850e094e63b45efedc116373be9c93db8
hvail <415195993@qq.com>
2019-09-04 14:36:47 +0800

    v2.1.13-fix npm lib

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const redis_host = process.env.REDIS_HOST || "112.74.57.39";
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.send("hello world");
+    res.send("hello world " + redis_host);
 });
 
 module.exports = router;
